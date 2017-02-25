dc4e22ccefcf83815d7fd7100febabed82f8af33
Peter O'Hara Adu <ohara.invent@gmail.com>
2017-02-25 19:55:23 +0000

    Return to using cdn for font-awesome and leaflet

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -5,10 +5,12 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
+  include Cloudinary::CarrierWave
+
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
-  storage :fog
+  # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -47,24 +49,5 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
-
-
-  # Cloudinary starts here
-  include Cloudinary::CarrierWave
-
-  process :convert => 'png'
-  process :tags => ['post_picture']
-
-  version :standard do
-    process :resize_to_fill => [100, 150, :north]
-  end
-
-  version :thumbnail do
-    resize_to_fit(50, 50)
-  end
-
-  def public_id
-    return model.short_name
-  end
 
 end
