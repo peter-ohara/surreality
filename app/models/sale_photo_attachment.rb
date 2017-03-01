fc3071c55c589fa1c67ef06b2a6c7928a590021d
Peter O'Hara Adu <ohara.invent@gmail.com>
2017-03-01 22:50:20 +0000

    Remove blank photo validation

--- app/models/sale_photo_attachment.rb
+++ app/models/sale_photo_attachment.rb
@@ -2,5 +2,5 @@ class SalePhotoAttachment < ApplicationRecord
   belongs_to :sale_listing, inverse_of: :sale_photo_attachments
   mount_uploader :photo, PhotoUploader
 
-  validates :photo, presence: true
+  # validates :photo, presence: true
 end
