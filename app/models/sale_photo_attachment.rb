c4b9281b84f8eb429f8b5864c5ed202fe1560fd5
Peter O'Hara Adu <ohara.invent@gmail.com>
2017-03-02 15:01:55 +0000

    Resize images to fit better

--- app/models/sale_photo_attachment.rb
+++ app/models/sale_photo_attachment.rb
@@ -3,4 +3,8 @@ class SalePhotoAttachment < ApplicationRecord
   mount_uploader :photo, PhotoUploader
 
   validates :photo, presence: true
+
+  def before_import_save(record)
+    self.remote_photo_url = record[:photo]['photo']['url'] if record[:photo].present?
+  end
 end
